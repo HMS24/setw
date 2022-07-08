c9d00547bf5d07f59afb50a8e1878139b9377f9f
chenrohua24 <z7707092003@gmail.com>
2022-07-08 22:50:19 +0800

    Refactor: 移除轉換 df 欄位型別的邏輯
    - 統一轉成 str ，之後使用 schema 做轉換

--- transform.py
+++ transform.py
@@ -8,42 +8,23 @@ def _translate_columns_languages(columns, mapper):
 def transform(df, date):
     df = df.copy()
     df.columns = _translate_columns_languages(df.columns, zh_en_map)
+
     df['dir'] = (df['dir']
                  .str.split('>')
                  .str[1]
                  .str[0])
-
     df['change'] = df['dir'] + df['change'].astype(str)
-
-    df['change'] = (df['change']
-                    .str.strip()
-                    .str.replace('X', '')
-                    .astype(float))
-
     df = df.drop('dir', axis=1)
 
-    for col in ('stock_id', 'stock_name'):
+    # note: 價格相關，例如開盤價會有 '--' 代表無意義數值，為了存資料庫並且計算方便 replace 為 '0'，
+    # 所有 col 先 to_str ，後續一起轉成對應的 type
+    for col in df.columns:
         df[col] = (df[col]
                    .astype(str)
-                   .str.strip())
-
-    for col in ('trade_volume', 'transaction', 'trade_value',
-                'last_best_bid_volume', 'last_best_ask_volume'):
-        df[col] = (df[col]
-                   .astype(str)
-                   .str.replace(',', '').
-                   astype(int))
-
-    # 價格部分 例如開盤價 會有 '--' 代表無意義
-    # 為了存資料庫計算方便 replace 為 0.00
-    for col in ('opening_price', 'highest_price', 'lowest_price',
-                'closing_price', 'last_best_bid_price', 'last_best_ask_price',
-                'price_earning_ratio'):
-        df[col] = (df[col]
-                   .astype(str)
+                   .str.strip()
                    .str.replace(',', '')
-                   .str.replace('--', '0')
-                   .astype(float))
+                   .str.replace('X', '')
+                   .str.replace('--', '0'))
 
     df = df.fillna('')
     df['date'] = date
